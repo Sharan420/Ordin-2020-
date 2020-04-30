6c6e98e3f19b7f701374943df2804517898f2a97
Naman Chandok <42838783+NamanChandok@users.noreply.github.com>
2020-04-30 21:40:37 +0530

    perhaps

---
+++ index.js
@@ -0,0 +1,39 @@
+    // DARK MODE
+    
+    const toggleSwitch = document.querySelector('.theme-switch input[type="checkbox"]');
+
+    function switchTheme(e) {
+        if (e.target.checked) {
+            document.documentElement.setAttribute('data-theme', 'dark');
+        }
+        else {
+            document.documentElement.setAttribute('data-theme', 'light');
+        }    
+    }
+    
+    // SAVE PREFERENCE TO LOCAL STORAGE
+
+    toggleSwitch.addEventListener('change', switchTheme, false);
+    function switchTheme(e) {
+        if (e.target.checked) {
+            document.documentElement.setAttribute('data-theme', 'dark');
+            localStorage.setItem('theme', 'dark');
+        }
+        else {
+            document.documentElement.setAttribute('data-theme', 'light');
+            localStorage.setItem('theme', 'light');
+        }    
+    }
+    
+    // CHECK LOCAL STORAGE FOR PREFERENCE
+    
+    const currentTheme = localStorage.getItem('theme') ? localStorage.getItem('theme') : null;
+
+        if (currentTheme) {
+            document.documentElement.setAttribute('data-theme', currentTheme);
+
+        if (currentTheme === 'dark') {
+            toggleSwitch.checked = true;
+            document.getElementById("skill-img").style.filter = "invert(100)";
+        }
+    }
