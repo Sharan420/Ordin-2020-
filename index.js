ba866d054727d2c84419bef72a7b62be4c1168c8
Naman Chandok <42838783+NamanChandok@users.noreply.github.com>
2020-05-02 17:46:01 +0530

    vjh

--- index.js
+++ index.js
@@ -1,39 +1,40 @@
     // DARK MODE
     
-    const toggleSwitch = document.querySelector('.theme-switch input[type="checkbox"]');
+const toggleSwitch = document.querySelector('.theme-switch input[type="checkbox"]');
 
-    function switchTheme(e) {
-        if (e.target.checked) {
-            document.documentElement.setAttribute('data-theme', 'dark');
-        }
-        else {
-            document.documentElement.setAttribute('data-theme', 'light');
-        }    
+function switchTheme(e) {
+    if (e.target.checked) {
+        document.documentElement.setAttribute('data-theme', 'dark');
     }
+    else {
+        document.documentElement.setAttribute('data-theme', 'light');
+    }    
+}
     
     // SAVE PREFERENCE TO LOCAL STORAGE
 
-    toggleSwitch.addEventListener('change', switchTheme, false);
-    function switchTheme(e) {
-        if (e.target.checked) {
-            document.documentElement.setAttribute('data-theme', 'dark');
-            localStorage.setItem('theme', 'dark');
-        }
-        else {
-            document.documentElement.setAttribute('data-theme', 'light');
-            localStorage.setItem('theme', 'light');
-        }    
+toggleSwitch.addEventListener('change', switchTheme, false);
+function switchTheme(e) {
+    if (e.target.checked) {
+        document.documentElement.setAttribute('data-theme', 'dark');
+        localStorage.setItem('theme', 'dark');
     }
+    else {
+        document.documentElement.setAttribute('data-theme', 'light');
+        localStorage.setItem('theme', 'light');
+    }    
+}
     
     // CHECK LOCAL STORAGE FOR PREFERENCE
     
-    const currentTheme = localStorage.getItem('theme') ? localStorage.getItem('theme') : null;
+const currentTheme = localStorage.getItem('theme') ? localStorage.getItem('theme') : null;
 
-        if (currentTheme) {
-            document.documentElement.setAttribute('data-theme', currentTheme);
+    if (currentTheme) {
+        document.documentElement.setAttribute('data-theme', currentTheme);
 
-        if (currentTheme === 'dark') {
-            toggleSwitch.checked = true;
-            document.getElementById("skill-img").style.filter = "invert(100)";
-        }
+    if (currentTheme === 'dark') {
+        toggleSwitch.checked = true;
+        document.getElementById("skill-img").style.filter = "invert(100)";
     }
+}
+
